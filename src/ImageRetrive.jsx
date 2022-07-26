410b06f67f9c519b897bd34ab2f1e962832ffdcc
Mathushan <mathuzan97@gmail.com>
2022-07-26 10:59:07 +0530

    Spinner related code removed

--- src/ImageRetrive.jsx
+++ src/ImageRetrive.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import { FadeLoader } from "react-spinners";
 
 
-// Adding page spinner to the page.
 const ImageRetrive = () => {
   useEffect(() => {
     getImageUrl();
@@ -12,12 +11,6 @@ const ImageRetrive = () => {
   const [loading, setLoading] = useState(false);
   const [post, setPost] = useState(null);
 
-  useEffect(() => {
-    setLoading(true);
-    setTimeout(() => {
-      setLoading(false);
-    }, 8000);
-  }, []);
 
   const getImageUrl = () => {
     var fetchUrl =
@@ -33,10 +26,6 @@ const ImageRetrive = () => {
         console.log(resjson);
         setImage(resjson.image);
       })
-      then(Response => {
-        const (body)=Response
-        setPost(body)
-      })
       .catch((error) => {
         console.error("Error:", error);
       });
@@ -45,11 +34,6 @@ const ImageRetrive = () => {
   return (
     <div>
       <img src={image} height={500} width={500} />
-
-      <FadeLoader size={50} color={"#123abc"} loading={loading} />
-      {post? post : <Backdrop className={classes.backdrop} open>
-        <FadeLoader size={50} color={"#123abc"} loading={loading} />
-      </Backdrop>}
     </div>
   );
 };
