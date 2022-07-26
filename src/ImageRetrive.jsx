c499cd9a76658a673bec18b9646b7461b0ce75b6
Mathushan <mathuzan97@gmail.com>
2022-07-26 12:39:33 +0530

    Adding a working repo- for 8 sec :

--- src/ImageRetrive.jsx
+++ src/ImageRetrive.jsx
@@ -1,18 +1,23 @@
 import React, { useEffect, useState } from "react";
 import { FadeLoader } from "react-spinners";
 
-
 const ImageRetrive = () => {
   useEffect(() => {
     getImageUrl();
   });
 
   const [image, setImage] = useState("");
-  const [loading, setLoading] = useState(false);
-  const [post, setPost] = useState(null);
+  const [ loading, setLoading] = useState(false);
 
+  useEffect(() => {
+    setLoading(true)
+    setTimeout(() => {
+      setLoading(false)
+    }, 8000)
+  }, [])
 
   const getImageUrl = () => {
+
     var fetchUrl =
       "https://cloudflare-ipfs.com/ipfs/QmfMKDqPWR833dMfGkW38usYS9GsGK2v1t3SF2aWrYZtHz";
     fetch(fetchUrl, {
@@ -34,8 +39,14 @@ const ImageRetrive = () => {
   return (
     <div>
       <img src={image} height={500} width={500} />
+      <FadeLoader
+      size={50}
+      color={"#123abc"}
+      loading={loading}
+      />
     </div>
   );
 };
 
 export default ImageRetrive;
+
